9fa9d11e5baf9d0d861710a3283aee71bc5edf58
Fahida-pk <118457841+Fahida-pk@users.noreply.github.com>
2026-06-01 16:34:22 +0530

    update project

--- src/pages/ProtectedRoute.jsx
+++ src/pages/ProtectedRoute.jsx
@@ -1,9 +1,11 @@
 import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children }) => {
-
   const token = sessionStorage.getItem("token");
   const loginTime = sessionStorage.getItem("loginTime");
+
+  console.log("Token:", token);
+  console.log("Login Time:", loginTime);
 
   const FIVE_MINUTES = 5 * 60 * 1000;
 
@@ -14,6 +16,8 @@ const ProtectedRoute = ({ children }) => {
 
   const currentTime = Date.now();
   const timeDifference = currentTime - Number(loginTime);
+
+  console.log("Time Difference:", timeDifference);
 
   if (timeDifference > FIVE_MINUTES) {
     sessionStorage.clear();
