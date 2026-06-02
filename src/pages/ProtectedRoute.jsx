4b3fef6aeddc5c2ddab74f9273a10fd2d420f3c7
Fahida-pk <118457841+Fahida-pk@users.noreply.github.com>
2026-06-02 12:18:55 +0530

    update project

--- src/pages/ProtectedRoute.jsx
+++ src/pages/ProtectedRoute.jsx
@@ -4,18 +4,13 @@ const ProtectedRoute = ({ children }) => {
   const token = sessionStorage.getItem("token");
   const loginTime = sessionStorage.getItem("loginTime");
 
-  console.log("Token:", token);
-  console.log("LoginTime:", loginTime);
-
   const FIVE_MINUTES = 5 * 60 * 1000;
 
   if (!token || !loginTime) {
-    console.log("No token or loginTime found");
     return <Navigate to="/" replace />;
   }
 
   if (Date.now() - Number(loginTime) > FIVE_MINUTES) {
-    console.log("Session Expired");
     sessionStorage.clear();
     return <Navigate to="/" replace />;
   }
