9ecfbb22729543abeabaf72ae778d8b02edffc50
Fahida-pk <118457841+Fahida-pk@users.noreply.github.com>
2026-06-02 11:35:36 +0530

    update project

--- src/pages/ProtectedRoute.jsx
+++ src/pages/ProtectedRoute.jsx
@@ -1,10 +1,27 @@
 import { Navigate } from "react-router-dom";
+import { useEffect } from "react";
 
 const ProtectedRoute = ({ children }) => {
   const token = sessionStorage.getItem("token");
   const loginTime = sessionStorage.getItem("loginTime");
 
   const FIVE_MINUTES = 5 * 60 * 1000;
+
+  useEffect(() => {
+    const updateActivity = () => {
+      sessionStorage.setItem("loginTime", Date.now().toString());
+    };
+
+    window.addEventListener("click", updateActivity);
+    window.addEventListener("mousemove", updateActivity);
+    window.addEventListener("keydown", updateActivity);
+
+    return () => {
+      window.removeEventListener("click", updateActivity);
+      window.removeEventListener("mousemove", updateActivity);
+      window.removeEventListener("keydown", updateActivity);
+    };
+  }, []);
 
   if (!token || !loginTime) {
     return <Navigate to="/" replace />;
