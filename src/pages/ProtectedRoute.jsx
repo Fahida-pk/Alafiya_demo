22facd8cf88f98514815b1db51ef0f1fa942c5a6
Fahida-pk <118457841+Fahida-pk@users.noreply.github.com>
2026-06-02 12:15:30 +0530

    update project

--- src/pages/ProtectedRoute.jsx
+++ src/pages/ProtectedRoute.jsx
@@ -1,33 +1,21 @@
 import { Navigate } from "react-router-dom";
-import { useEffect } from "react";
 
 const ProtectedRoute = ({ children }) => {
   const token = sessionStorage.getItem("token");
   const loginTime = sessionStorage.getItem("loginTime");
 
+  console.log("Token:", token);
+  console.log("LoginTime:", loginTime);
+
   const FIVE_MINUTES = 5 * 60 * 1000;
 
-  useEffect(() => {
-    const updateActivity = () => {
-      sessionStorage.setItem("loginTime", Date.now().toString());
-    };
-
-    window.addEventListener("click", updateActivity);
-    window.addEventListener("mousemove", updateActivity);
-    window.addEventListener("keydown", updateActivity);
-
-    return () => {
-      window.removeEventListener("click", updateActivity);
-      window.removeEventListener("mousemove", updateActivity);
-      window.removeEventListener("keydown", updateActivity);
-    };
-  }, []);
-
   if (!token || !loginTime) {
+    console.log("No token or loginTime found");
     return <Navigate to="/" replace />;
   }
 
   if (Date.now() - Number(loginTime) > FIVE_MINUTES) {
+    console.log("Session Expired");
     sessionStorage.clear();
     return <Navigate to="/" replace />;
   }
