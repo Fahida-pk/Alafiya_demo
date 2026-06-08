3667bbb5a9c0e07eb188b5ff2b70942eed782e53
Fahida-pk <118457841+Fahida-pk@users.noreply.github.com>
2026-06-08 13:40:01 +0530

    update project

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -10,59 +10,56 @@ const Login = () => {
   const navigate = useNavigate();
 
   const handleLogin = async (e) => {
-    e.preventDefault();
-    setError("");
+  e.preventDefault();
+  setError("");
 
-    try {
-      const res = await fetch(
-        "https://zyntaweb.com/demoalafiya/api/login.php",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            username,
-            password,
-          }),
-        }
-      );
+  try {
+    const res = await fetch(
+      "https://zyntaweb.com/demoalafiya/api/login.php",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username,
+          password,
+        }),
+      }
+    );
 
     const data = await res.json();
-console.log(data);
+    console.log(data);
 
-if (data.status === "success") {
+    if (data.status === "success") {
+      console.log("Login Success");
 
-  console.log("Login Success");
+      sessionStorage.setItem("token", "loggedin");
+      sessionStorage.setItem("user_id", data.user_id);
+      sessionStorage.setItem("role", data.role);
 
-  sessionStorage.setItem("token", "loggedin");
-  sessionStorage.setItem("user_id", data.user_id);
-  sessionStorage.setItem("role", data.role);
+      console.log(
+        "Saved User ID:",
+        sessionStorage.getItem("user_id")
+      );
 
-  console.log(
-    "Saved User ID:",
-    sessionStorage.getItem("user_id")
-  );
+      sessionStorage.setItem(
+        "loginTime",
+        Date.now().toString()
+      );
 
-  sessionStorage.setItem(
-    "loginTime",
-    Date.now().toString()
-  );
+      console.log("Before Navigate");
 
-  alert("Login Success");
+      navigate("/dashboard");
+    } else {
+      setError("Invalid username or password");
+    }
 
-  navigate("/dashboard");
-
-} else {
-  setError("Invalid username or password");
-}
-
-} catch (error) {
-  console.log(error);
-  setError("Server error. Try again");
-}
+  } catch (error) {
+    console.log(error);
+    setError("Server error. Try again");
+  }
 };
-
   return (
     <div className="login-wrapper">
       <form className="login-content" onSubmit={handleLogin}>
